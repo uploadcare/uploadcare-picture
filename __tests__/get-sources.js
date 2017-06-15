5c38bb1d11c846d93a12ebef984faebfebb5b765
Alexander Kurganov <me@akurganow.ru>
2017-06-15 18:53:30 +0300

    get-sources

--- __tests__/get-sources.js
+++ __tests__/get-sources.js
@@ -1,5 +1,184 @@
+import {getSources} from '../modules/get-sources'
+
+const uuid = '18d1c520-c52d-4c34-82a0-7e07dcbcf105'
+const cdnurl = `https://ucarecdn.com/${uuid}/`
+
 describe('getSources', () => {
-  test('empty', () => {
-    expect(false).toBe(true)
+  test('undefined options', () => {
+    const expected = null
+
+    expect(getSources(uuid)).toEqual(expected)
+  })
+  test('empty options', () => {
+    const options = {}
+    const expected = null
+
+    expect(getSources(uuid, options)).toEqual(expected)
+  })
+  test('one format', () => {
+    const options = {formats: ['jpg']}
+    const expected = [
+      {
+        src: `${cdnurl}-/format/jpg/`,
+        type: 'image/jpg',
+      },
+    ]
+
+    expect(getSources(uuid, options)).toEqual(expected)
+  })
+  test('one size', () => {
+    const options = {sizes: {'(max-width: 1024px)': 768}}
+    const expected = [
+      {
+        src: `${cdnurl}-/resize/768x/-/format/auto/`,
+        srcset: `${cdnurl}-/resize/1536x/-/format/auto/ 2x`,
+        media: '(max-width: 1024px)',
+        sizes: 768,
+      },
+    ]
+
+    expect(getSources(uuid, options)).toEqual(expected)
+  })
+  test('one format and one size', () => {
+    const options = {
+      formats: ['jpg'],
+      sizes: {'(max-width: 1024px)': 768},
+    }
+    const expected = [
+      {
+        src: `${cdnurl}-/resize/768x/-/format/jpg/`,
+        srcset: `${cdnurl}-/resize/1536x/-/format/jpg/ 2x`,
+        media: '(max-width: 1024px)',
+        type: 'image/jpg',
+        sizes: 768,
+      },
+    ]
+
+    expect(getSources(uuid, options)).toEqual(expected)
+  })
+  test('two formats and one size', () => {
+    const options = {
+      formats: ['jpg', 'webp'],
+      sizes: {'(max-width: 1024px)': 768},
+    }
+    const expected = [
+      {
+        src: `${cdnurl}-/resize/768x/-/format/jpg/`,
+        srcset: `${cdnurl}-/resize/1536x/-/format/jpg/ 2x`,
+        media: '(max-width: 1024px)',
+        type: 'image/jpg',
+        sizes: 768,
+      },
+      {
+        src: `${cdnurl}-/resize/768x/-/format/webp/`,
+        srcset: `${cdnurl}-/resize/1536x/-/format/webp/ 2x`,
+        media: '(max-width: 1024px)',
+        type: 'image/webp',
+        sizes: 768,
+      },
+    ]
+
+    expect(getSources(uuid, options)).toEqual(expected)
+  })
+  test('one format and two sizes', () => {
+    const options = {
+      formats: ['jpg'],
+      sizes: {
+        '(max-width: 1024px)': 768,
+        '(max-width: 1280px)': 1024,
+      },
+    }
+    const expected = [
+      {
+        src: `${cdnurl}-/resize/768x/-/format/jpg/`,
+        srcset: `${cdnurl}-/resize/1536x/-/format/jpg/ 2x`,
+        media: '(max-width: 1024px)',
+        type: 'image/jpg',
+        sizes: 768,
+      },
+      {
+        src: `${cdnurl}-/resize/1024x/-/format/jpg/`,
+        srcset: `${cdnurl}-/resize/2048x/-/format/jpg/ 2x`,
+        media: '(max-width: 1280px)',
+        type: 'image/jpg',
+        sizes: 1024,
+      },
+    ]
+
+    expect(getSources(uuid, options)).toEqual(expected)
+  })
+  test('three formats and three sizes', () => {
+    const options = {
+      formats: ['jpg', 'webp', 'png'],
+      sizes: {
+        '(max-width: 1024px)': 768,
+        '(max-width: 1280px)': 1024,
+        'default': 1280,
+      },
+    }
+    const expected = [
+      {
+        src: `${cdnurl}-/resize/768x/-/format/jpg/`,
+        srcset: `${cdnurl}-/resize/1536x/-/format/jpg/ 2x`,
+        media: '(max-width: 1024px)',
+        type: 'image/jpg',
+        sizes: 768,
+      },
+      {
+        src: `${cdnurl}-/resize/1024x/-/format/jpg/`,
+        srcset: `${cdnurl}-/resize/2048x/-/format/jpg/ 2x`,
+        media: '(max-width: 1280px)',
+        type: 'image/jpg',
+        sizes: 1024,
+      },
+      {
+        src: `${cdnurl}-/resize/1280x/-/format/jpg/`,
+        srcset: `${cdnurl}-/resize/2560x/-/format/jpg/ 2x`,
+        type: 'image/jpg',
+        sizes: 1280,
+      },
+      {
+        src: `${cdnurl}-/resize/768x/-/format/webp/`,
+        srcset: `${cdnurl}-/resize/1536x/-/format/webp/ 2x`,
+        media: '(max-width: 1024px)',
+        type: 'image/webp',
+        sizes: 768,
+      },
+      {
+        src: `${cdnurl}-/resize/1024x/-/format/webp/`,
+        srcset: `${cdnurl}-/resize/2048x/-/format/webp/ 2x`,
+        media: '(max-width: 1280px)',
+        type: 'image/webp',
+        sizes: 1024,
+      },
+      {
+        src: `${cdnurl}-/resize/1280x/-/format/webp/`,
+        srcset: `${cdnurl}-/resize/2560x/-/format/webp/ 2x`,
+        type: 'image/webp',
+        sizes: 1280,
+      },
+      {
+        src: `${cdnurl}-/resize/768x/-/format/png/`,
+        srcset: `${cdnurl}-/resize/1536x/-/format/png/ 2x`,
+        media: '(max-width: 1024px)',
+        type: 'image/png',
+        sizes: 768,
+      },
+      {
+        src: `${cdnurl}-/resize/1024x/-/format/png/`,
+        srcset: `${cdnurl}-/resize/2048x/-/format/png/ 2x`,
+        media: '(max-width: 1280px)',
+        type: 'image/png',
+        sizes: 1024,
+      },
+      {
+        src: `${cdnurl}-/resize/1280x/-/format/png/`,
+        srcset: `${cdnurl}-/resize/2560x/-/format/png/ 2x`,
+        type: 'image/png',
+        sizes: 1280,
+      },
+    ]
+
+    expect(getSources(uuid, options)).toEqual(expected)
   })
 })
--- modules/get-sources.js
+++ modules/get-sources.js
@@ -1,11 +1,96 @@
+import {buildOptions} from './build-options'
+import {buildSrc} from './build-src'
+import {buildSrcSet} from './build-srcset'
+import {cleanKeys} from './clean-keys'
+
+/**
+ * [buildWidth description]
+ * @param  {[type]} sizes [description]
+ * @return {[type]}       [description]
+ */
+export function buildWidth(sizes) {
+  return sizes
+}
+
+/**
+ * [buildSource description]
+ * @param  {[type]} options [description]
+ * @return {[type]}         [description]
+ */
+export function buildSource(uuid, options) {
+  const src = buildSrc(uuid, {
+    width: buildWidth(options.sizes),
+    oversize: options.oversize,
+    max_resize: options.max_resize,
+    format: options.format,
+  })
+  const srcset = buildSrcSet(uuid, {
+    src,
+    width: buildWidth(options.sizes),
+    pixel_density: options.pixel_density,
+    oversize: options.oversize,
+    max_resize: options.max_resize,
+    format: options.format,
+  })
+  const source = {
+    type: options.format ? `image/${options.format}` : null,
+    media: options.media === 'default'
+      ? null
+      : options.media,
+    sizes: options.sizes,
+    src,
+    srcset,
+  }
+
+  return cleanKeys(source)
+}
+
 /**
  * [getSources description]
  * @param  {String} uuid
  * @param  {Object} opts
  * @return {Object}
  */
-function getSources(uuid, opts) {
-  return null
-}
+export function getSources(uuid, opts) { // eslint-disable-line max-statements
+  if (!opts || Object.keys(opts).length <= 0) return null
+  if (!opts.sizes && !opts.formats) return null
 
-export {getSources}
+  const options = buildOptions(opts)
+  const sources = []
+  const formatsLength = options.formats ? options.formats.length : 1
+  const sizesKeys = options.sizes ? Object.keys(options.sizes) : []
+  const sizesLength = sizesKeys.length || 1
+
+  let formatsIndex = 0
+  let sizesIndex = 0
+  let sourcesLength = formatsLength * sizesLength
+
+  for (let ind = 0; ind < sourcesLength; ind++) {
+    sources.push(buildSource(uuid, {
+      format: options.formats ? options.formats[formatsIndex] : null,
+      media: options.sizes ? sizesKeys[sizesIndex] : null,
+      sizes: options.sizes ? options.sizes[sizesKeys[sizesIndex]] : null,
+      pixel_density: options.pixel_density,
+      oversize: options.oversize,
+      max_resize: options.max_resize,
+    }))
+
+    if (sizesIndex < sizesLength - 1) {
+      sizesIndex++
+    }
+    else {
+      sizesIndex = 0
+
+      if (formatsIndex < formatsLength - 1) {
+        formatsIndex++
+      }
+      else {
+        formatsIndex = 0
+      }
+    }
+  }
+
+  // console.log(sources)
+
+  return sources.length > 0 ? sources : null
+}
