bb9876eee07a18816907c136e6f9adcbe1380fb0
Alexander Kurganow <me@akurganow.ru>
2018-02-24 17:14:25 +0300

    Update jsx adapter

--- adapters/jsx.js
+++ adapters/jsx.js
@@ -1,34 +1,34 @@
-import React, {Component} from 'react'
+import React from 'react'
 import getPictureObject from '../index'
 
-class Picture extends Component {
-  renderSources = (sources) => sources.map(this.renderSource)
+const renderSource = source => (
+  <source
+    key={source.srcset}
+    srcSet={source.srcset}
+    width={source.width}
+    sizes={source.sizes}
+    media={source.media}
+    type={source.type} />
+)
+const renderSources = sources => sources.map(renderSource)
+const renderImage = image => (
+  <img
+    src={image.src}
+    srcSet={image.srcset}
+    width={image.width}
+    sizes={image.sizes}
+    alt={image.alt} />
+)
 
-  renderSource = (source) =>
-    <source
-      key={source.srcset}
-      srcSet={source.srcset}
-      width={source.width}
-      sizes={source.sizes}
-      media={source.media}
-      type={source.type} />
+const Picture = ({uuid, ...rest}) => {
+  const tree = getPictureObject(uuid, rest)
 
-  renderImage = (image) =>
-    <img
-      src={image.src}
-      srcSet={image.srcset}
-      width={image.width}
-      sizes={image.sizes}
-      alt={image.alt} />
-
-  render() {
-    const tree = getPictureObject(this.props.uuid, this.props)
-
-    return <picture>
-      {this.renderSources(tree.sources)}
-      {this.renderImage(tree.image)}
+  return (
+    <picture>
+      {renderSources(tree.sources)}
+      {renderImage(tree.image)}
     </picture>
-  }
+  )
 }
 
 export default Picture
